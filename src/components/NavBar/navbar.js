e66f94c19f736e77bab18c20770167f9c10addb7
vansh kushwaha <108723120+vanshkushwaha8@users.noreply.github.com>
2023-12-28 11:31:49 +0530

    Update navbar.js

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo (2).png';
 import Contact from '../../assets/contact.png';
 import { Link } from 'react-scroll';
 import menubar from '../../assets/menu.png';
